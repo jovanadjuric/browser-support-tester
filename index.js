9a3d93ab5f6eb1011c1f3cefeaa8f6fc65a1d8f4
jovanamatk <mat.jovana@gmail.com>
2022-12-07 15:19:50 +0100

    .

---
+++ index.js
@@ -0,0 +1,17 @@
+// domcontentloaded event
+document.addEventListener('DOMContentLoaded', function () {
+    const currentBrowserSupportsPush = () => {
+        // if (!'PushManager' in window) { return false; }
+        // if (!'serviceWorker' in navigator) { return false; }
+        if (!'Notification' in window) { return false; }
+        return true;
+    }
+
+    const pushElement = document.getElementById('isPushSupported');
+
+    if (currentBrowserSupportsPush()) {
+        pushElement.innerHTML = 'Push is supported';
+    } else {
+        pushElement.innerHTML = 'Push is not supported';
+    }
+});
